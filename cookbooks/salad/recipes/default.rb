d486b0b6e536b352d93a9270fc67bc02dfd8f604
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-03-14 11:46:56 -0700

    Experimenting with chef_handler to no avail

--- cookbooks/salad/recipes/default.rb
+++ cookbooks/salad/recipes/default.rb
@@ -4,3 +4,8 @@ end
 
 # FIXME: when moving this LWRP to the jenkins cookbooks, the name will look better
 salad_jenkins_tracking "/tmp/bar.txt"
+
+# TODO: is this another way to install a handler?
+# chef_gem 'chef-handler-jenkins'  or something like that
+#include_recipe 'chef_handler'
+#chef_handler "MyHandler"
--- solo.rb
+++ solo.rb
@@ -1,12 +1,12 @@
 cookbook_path Dir.pwd+"/cookbooks"
 role_path Dir.pwd+"/roles"
-
+data_bag_path Dir.pwd+"/databags"
 
 # install custom handler
 # this code can live in client.rb, too
 # TODO: if I need to configure which HTTP URL to send the report to, how should I do that?
-require 'lib/myhandler'
+require 'cookbooks/salad/libraries/myhandler'
 report_handlers << MyHandler.new
 
-require 'chef/handler/json_file.rb'
-report_handlers << Chef::Handler::JsonFile.new
+#require 'chef/handler/json_file.rb'
+#report_handlers << Chef::Handler::JsonFile.new
