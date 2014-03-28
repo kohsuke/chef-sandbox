49c56fc9dd43a6dbe31f04967c296d87a74036c7
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-03-28 14:13:26 -0700

    Packaging into hash for machine submission

--- cookbooks/salad/recipes/default.rb
+++ cookbooks/salad/recipes/default.rb
@@ -3,7 +3,7 @@ cookbook_file "/tmp/foo.txt" do
 end
 
 # FIXME: when moving this LWRP to the jenkins cookbooks, the name will look better
-#salad_jenkins_tracking "/tmp/bar.txt"
+salad_jenkins_tracking "/tmp/bar.txt"
 
 # TODO: is this another way to install a handler?
 # chef_gem 'chef-handler-jenkins'  or something like that
--- lib/chef/handler/jenkins.rb
+++ lib/chef/handler/jenkins.rb
@@ -8,24 +8,32 @@ module Jenkins
       # for interactive exploration
       # binding.pry
 
+      report = []
+
       puts "Machine name: #{run_status.node.name}"
       run_status.updated_resources.each do |res|
         # res is instance of CookbookFile
         if res.class <= Chef::Resource::File
-          puts "Updated file"
-          puts "  path = #{res.path}"
-          puts "  action = #{res.action}"
-          puts "  md5 = #{Digest::MD5.hexdigest(IO.read(res.path))}"
+          report << {
+            "path" => res.path,
+            "action" => res.action,
+            "md5" => Digest::MD5.hexdigest(IO.read(res.path)),
+            "type" => res.class.name
+          }
           # res.checksum is SHA1 sum
         end
 
         if res.class == Chef::Resource::SaladJenkinsTracking
           # TODO is this a good way to check the class name?
-          puts "Tracked"
-          puts "  path = #{res.path}"
-          puts "  md5 = #{res.checksum}"
+          report << {
+              "path" => res.path,
+              "md5" => res.checksum,
+              "type" => res.class.name
+          }
         end
       end
+
+      print report.inspect
     end
   end
 end
