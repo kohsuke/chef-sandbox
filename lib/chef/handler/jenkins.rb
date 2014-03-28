3c0930f752e39479130d896b589e8c2baeaa7cc9
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-03-28 14:04:16 -0700

    better subtyping check

--- lib/chef/handler/jenkins.rb
+++ lib/chef/handler/jenkins.rb
@@ -11,12 +11,12 @@ module Jenkins
       puts "Machine name: #{run_status.node.name}"
       run_status.updated_resources.each do |res|
         # res is instance of CookbookFile
-        if res.class==Chef::Resource::CookbookFile
+        if res.class <= Chef::Resource::File
           puts "Updated file"
           puts "  path = #{res.path}"
           puts "  action = #{res.action}"
           puts "  md5 = #{Digest::MD5.hexdigest(IO.read(res.path))}"
-          # TODO: what is res.checksum?
+          # res.checksum is SHA1 sum
         end
 
         if res.class == Chef::Resource::SaladJenkinsTracking
